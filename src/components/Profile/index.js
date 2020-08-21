6386d863c63489a0b2b0286f41abce9c2cef4b4f
Reinaldo <reinaldo800@gmail.com>
2020-08-21 19:09:56 -0300

    Componente Profile

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Reinaldo P Jr</h1>
+    <h2>Programador Junior FullStack</h2>
+    <p>Labore qui sint Lorem do proident.</p>
+  </div>
+)
+
+export default Profile
