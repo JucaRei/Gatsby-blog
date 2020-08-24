245e8e0a8f496a2dbc0ba2e40b890c9f12392295
Reinaldo <reinaldo800@gmail.com>
2020-08-24 14:52:11 -0300

    useStaticQuery react Hooks

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,28 +1,68 @@
-import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'  // buscar dados usando o staticQuery
+import React from "react"
+//import { StaticQuery, graphql } from "gatsby" // buscar dados usando o staticQuery via graphql
+import { useStaticQuery, graphql } from "gatsby" // usando react hooks
 
-const Profile = () => (
-  <StaticQuery
-      query={graphql`
-      query MySiteMetadata {
-        site {
-          siteMetadata {
-            title
-            position
-            description
-          }
+const Profile = () => {
+  const {
+    site: {
+      siteMetadata: { title, position, description },
+    },
+  } = useStaticQuery(graphql`
+    query MySiteMetadata {
+      site {
+        siteMetadata {
+          title
+          position
+          description
         }
       }
-    `}
-    render={({site: {siteMetadata: { title, position, description}}}) => (
-      // destructuring
-      <div className="Profile-wrapper">
-        <h1>{title}</h1>
-        <h2>{position}</h2>
-        <p>{description}</p>
-      </div>
-    )}
-  />
-)
+    }
+  `)
+
+  // renderizar esse dados
+  return (
+    <div className="Profile-wrapper">
+      <h1>{title}</h1>
+      <h2>{position}</h2>
+      <p>{description}</p>
+    </div>
+  )
+}
+// query igual a IDE do graphql
+// const Profile = () => (
+//   <StaticQuery
+//     query={graphql`
+//       query MySiteMetadata {
+//         site {
+//           siteMetadata {
+//             title
+//             position
+//             description
+//           }
+//         }
+//       }
+//     `}
+// um dos jeitos de fazer a requisição
+// render={data=> (
+//   <div className="Profile-wrapper">
+//     <h1>{data.site.siteMetadata.title}</h1>
+//     <h2>{data.site.siteMetadata.position}</h2>
+//     <p>{data.site.siteMetadata.author}</p>
+//   </div>
+// )}
+
+//outro utilizando o destructuring, para ter menos repetição
+//   render={({
+//     site: {
+//       siteMetadata: { title, position, description },
+//     },
+//   }) => (
+//     <div className="Profile-wrapper">
+//       <h1>{title}</h1>
+//       <h2>{position}</h2>
+//       <p>{description}</p>
+//     </div>
+//   )}
+// />
 
 export default Profile
