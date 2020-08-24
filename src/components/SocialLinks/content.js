dc7a9095273b828311ede1b59280cda93ef7c931
Reinaldo <reinaldo800@gmail.com>
2020-08-24 20:14:09 -0300

    Colocando os icones com o styled-icons

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,24 @@
+const links = [
+  {
+    label: "Github",
+    url: "https://github.com/JucaRei",
+  },
+  {
+    label: "Twitter",
+    url: "https://twitter.com/",
+  },
+  {
+    label: "Youtube",
+    url: "https://www.youtube.com/",
+  },
+  {
+    label: "Instagram",
+    url: "https://www.instagram.com.br",
+  },
+  {
+    label: "Unsplash",
+    url: "https://unsplash.com",
+  },
+]
+
+export default links
---
+++ src/components/SocialLinks/icons.js
@@ -0,0 +1,15 @@
+import { Github } from "styled-icons/boxicons-logos/Github"
+import { Twitter } from "styled-icons/boxicons-logos/Twitter"
+import { Youtube } from "styled-icons/boxicons-logos/Youtube"
+import { Unsplash } from "styled-icons/remix-line/Unsplash"
+import { Instagram } from "styled-icons/remix-fill/Instagram"
+
+const Icons = {
+  Github,
+  Twitter,
+  Youtube,
+  Unsplash,
+  Instagram,
+}
+
+export default Icons
