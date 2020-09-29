1e1797ac01939b3ce3d22c2ae0c6e2e0c1fd37bb
Reinaldo <reinaldo800@gmail.com>
2020-09-29 17:37:38 -0300

    Implementando lazyload com blur, etc...

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,4 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
 // Passar toda variável/biblioteca que queira passar na parte do front-end
+// Externo ao react
+
+import "lazysizes"
